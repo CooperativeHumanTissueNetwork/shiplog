bf107e413c9210c5c4c1971b672f8c8f6b366172
Zachery vm <zackvm.zv@gmail.com>
2017-04-28 16:49:04 -0400

    First Release Client v1.0

--- controller.php
+++ controller.php
@@ -4,26 +4,34 @@ error_reporting(0);
 // Report all PHP errors
 //error_reporting(-1);
 include 'getters.php';
-
-
 $myfile = fopen("/root/shiplog/log/logfile_" . date('YmdHi') . ".log", "w") or die("Unable to open file!");
-$txt = "Data Push Began: " . date('Y-m-d H:i') . "\n";
-$txt .= "------------------------------------------\n";
-$txt .= "| 1) GET RECORDS NEEDING UPLOAD          |\n";
-$txt .= "| 2) CYCLE THROUGH THE RECORDS           |\n";
-$txt .= "| 3) SEND THE RECORD TO CHTN.APP         |\n";
-$txt .= "| 4) CHECK RESPONSE FROM CHTN.APP        |\n";
-$txt .= "| 5) IF GOOD MARK RECORDS UPLOADED       |\n";
-$txt .= "------------------------------------------\n";
+$txt = "Data Push Began: " . date('Y-m-d H:i') . "\n------------------------------------------\n| 1) GET RECORDS NEEDING UPLOAD          |\n| 2) CYCLE THROUGH THE RECORDS           |\n| 3) SEND THE RECORD TO CHTN.APP         |\n| 4) CHECK RESPONSE FROM CHTN.APP        |\n| 5) IF GOOD MARK RECORDS UPLOADED       |\n------------------------------------------\n";
 fwrite($myfile, $txt);
-
+$txt = "[1] RETRIEVE UPLOADABLE RECORDS\n-----------------------------------\n";
+fwrite($myfile, $txt);
 $dSets = new datasets();
 $rtnVal = $dSets->getreconciledrecords();
 $data = json_decode($rtnVal, true);
-
+$txt = "[2-5] CYCLE RECORDS AND WRITE TO INVESTIGATOR+ AND MARK FINISHED\n-----------------------------------\n";
+fwrite($myfile, $txt);
 for ($i = 0, $l = count($data['DATA']); $i < $l; $i++) { 
-    fwrite($myfile, $data['DATA'][$i]['assignedReq'] . "  " . $data['DATA'][$i]['uploadNote'] . "\n");
+    $payloadData = array('requestId' => $data['DATA'][$i]['assignedReq']
+                    , 'shipCount' => (int)$data['DATA'][$i]['qty']
+                    , 'comments' => $data['DATA'][$i]['uploadNote']
+                    , 'shipDate' => $data['DATA'][$i]['shippedDate']
+                    , 'preparationId' => strtolower($data['DATA'][$i]['tvcode'])
+                    , 'anatomicSiteId' => $data['DATA'][$i]['siteid']
+                    , 'specimenTypeId' => $data['DATA'][$i]['catid']
+    );
+    $jStr = json_encode($payloadData);
+    echo $jStr . "\n";
+    fwrite($myfile, $jStr . "\n");
+    $collection = $dSets->putchtnapprecord($jStr);
+    if ($collection === "{\"shiplogs\":null,\"message\":null}") { 
+        $sdupdater = $dSets->markuploaded($data['DATA'][$i]['shipdocrefid']);
+        fwrite($myfile, $sdupdater . "\n");
+    }
 }
+fwrite($myfile, "\n----------------------\nFINISHED: " .  date('Y-m-d H:i'));
 fclose($myfile);
 exit;
-
--- getters.php
+++ getters.php
@@ -7,6 +7,17 @@ class datasets {
         return $ws;
     }
     
+    function putchtnapprecord($thisJSON) {
+        include 'auth.zck';
+        $ws = callRestAPI('PUT', 'https://chtn.app.vumc.org/investigatorplus/rest/communicationlog/shiplog/' . $invplusEast,$thisJSON);
+        return $ws;
+    }
+    
+    function markuploaded($whichSD) { 
+        $ws = callRestAPI('GET','https://www.chtneast.org/fourapi/reconciliation/shiplogupdate/' . $whichSD);
+        return $ws;
+    }
+    
 }
 
 
@@ -16,11 +27,18 @@ function callRestAPI($method, $url, $data = false) {
         $ch = curl_init();
         if (FALSE === $ch) throw new Exception('failed to initialize');
         switch ($method) { 
+            case "PUT":
+                //curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data)));
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                
+                break;
             case "POST":
-            curl_setopt($ch, CURLOPT_POST, 1);
-            if ($data) {
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            }
+                curl_setopt($ch, CURLOPT_POST, 1);
+                if ($data) {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                }
             break;
             case "GET":
                 curl_setopt($ch, CURLOPT_GET, 1);
